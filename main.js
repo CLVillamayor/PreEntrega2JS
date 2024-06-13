a8a576b2d5c1302dbe91db53e9d2c9c877a16291
Camila Villamayor <camilalvillamayor@gmail.com>
2024-06-12 21:53:44 -0300

    Verificacion de compra

--- main.js
+++ main.js
@@ -1,30 +1,46 @@
-//solicito nombre al usuario//
+//Inicializacion de las variables a utilizar
+let nombreUsuario = '';
+let producto = '0';
+let productoMenu = '';
+let cantidadMenu ='0';
+let precio = '0';
+let cantidadTotalMenuSelec = '0';
+let precioTotalMenSelec = '0';
+let productoPostre ='';
+let cantidadHelado = '0';
+let cantidadTotalHeladoSel = '0';
+let precioTotalHelSelec = '0';
+let CantidadTotaldeProductos = '0';
+let precioTotal = '0';
+let precioMenu ='0';
+let precioHelado = '0';
+let deseaSeguirComprando = false;
+let descuentoPromo = '0';
+let descuento = 0.25; 
+let precioPromo = '0';
+let precioTotalcp = '0';
+let cantidadTotMenuSolo = '0';
+let precioTotalPromoyHelado = '0';
 
 
-function solicitarNombre(){
-    let nombreUsuario = prompt("Para un servicio personalizado por favor ingrese su nombre")
-    alert("Hola "+ nombreUsuario );
-}
+//Ejecución del programa
 
+nombreUsuario = prompt('Para una atencion personalizada, por favor ingrese su nombre');
 
-//solicitarNombre();
-
-//do{
-    
-    solicitarNombre();
-    producto = prompt('qué deseas compar? A, :MENU, B, :CONO DE HELADO');
+do{
+    producto = prompt('Hola '  +   nombreUsuario,'qué deseas compar? A, :MENU, B, :CONO DE HELADO');
     switch(producto){
         case'A':
-        producto = prompt('El menu es 1,:Pizza, 2,:Milanesa, 3,:Hamburguesa, ');
+        productoMenu = prompt('El menu es Pizza, Milanesa, Hamburguesa, Empanada de Carne');
         cantidadMenu = parseInt(prompt('Ingrese la cantidad de menues a comprar'));
         switch(productoMenu){
-            case'1':
+            case'Pizza':
             precio= 10000;
             break;
-            case'2':
+            case'Milanesa':
             precio = 5000;
             break;
-            case'3':
+            case'Hamburguesa':
             precio = 7000;
             break;
             case 'Empanada de Carne':
@@ -77,41 +93,23 @@ function solicitarNombre(){
         
         break
     }
+    CantidadTotaldeProductos = cantidadTotalMenuSelec + cantidadTotalHeladoSel ;
+    precioTotal = precioTotalMenSelec + precioTotalHelSelec ;
+    precioTotalcp = precioPromo ;
+    precioTotalPromoyHelado = precioPromo + precioTotalHelSelec ;
+    cantidadTotMenuSolo = cantidadMenu ;
+    deseaSeguirComprando = confirm('Desea seguir comprando?');
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//}
-
-//const menu1= {
-    //nombre: "Pizza",
-    //precio: 10000,
-
-
-
-//console.log(menu1.precio)
-//console.log(menu1.nombre)
-
-//const arrayM = [{id: 1,producto: "Pizza", precio: 10},{id: 2, producto: "Milanesa", precio: 20},{id:3, producto: "Hamburguesa", precio: 30}];
-//for (cons producto of productos) {
-    console.log(producto.id);
-    console.log(producto.producto);
-
-//}
+}while(deseaSeguirComprando)
+    if (cantidadMenu>=6&& cantidadTotalHeladoSel==0) {
+        alert(nombreUsuario + ' tu compra ha sido exitosa, has comprado ' + cantidadTotMenuSolo + ' productos y el total a pagar es : $' + precioTotalcp);
+    } else if (cantidadMenu>=6&&  cantidadTotalHeladoSel>=1){
+        alert(nombreUsuario + ' tu compra ha sido exitosa, has comprado ' + CantidadTotaldeProductos + ' productos y el total a pagar es : $' + precioTotalPromoyHelado); 
+    } else if (cantidadMenu !=0){
+        alert(nombreUsuario + ' tu compra ha sido exitosa, has comprado ' + cantidadTotMenuSolo + ' productos y el total a pagar es : $' + precioTotalMenSelec); 
+    } else if (cantidadHelado !=0){
+        alert(nombreUsuario + ' tu compra ha sido exitosa, has comprado ' + cantidadTotalHeladoSel + ' productos y el total a pagar es : $' + precioTotalHelSelec); 
+    } else {
+        alert(nombreUsuario + ' tu compra ha sido exitosa, has comprado ' + CantidadTotaldeProductos + ' productos y el total a pagar es : $' + precioTotal); 
+    }
